6fa8d3476444e03aa7f3a6a240101e9c52af9399
Bradan Craig <bradancraig2004@gmail.com>
2025-10-21 14:34:59 -0400

    forgot one

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,9 @@ from pymongo import MongoClient
 import os
 
 app = Flask(__name__)
-client = MongoClient()
+client = MongoClient(os.environ.get("DATABASE_URI"))
+user_db = client["Users"]
+users_col = user_db["user_collection"]
+
 if __name__ == "__main__":
     app.run(debug=os.environ.get("FLASK_DEBUG"), port=5000)
