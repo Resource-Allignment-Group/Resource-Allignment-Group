2724d593d8c71f63c2c9614e173016af3f472cbc
Bradan Craig <bradancraig2004@gmail.com>
2025-10-21 20:09:39 -0400

    base log in workibng, not connected to databse yet, buut not hard to add

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,22 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/login";
 import Home from "./pages/home";
-
+import { AuthProvider } from './Authentication';
 function App() {
-  return (<Router>
+  return (
+  <Router>
+    <AuthProvider>
     <Routes>
       <Route path='/' element={<Navigate to='/login'/>} />
 
       <Route path="/login" element={<Login />} />
       <Route path="/home" element={<Home />} />
     {/*Will make the home route protected in a later commit*/}
-    
+
     </Routes>
+      </AuthProvider>
   </Router>
+  
   );
 }
 
