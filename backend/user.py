78d958b7befbbda40f1024c3ad5783e5d2f4eb34
Bradan Craig <bradancraig2004@gmail.com>
2025-11-11 15:13:54 -0500

    un tested setters added

--- backend/user.py
+++ backend/user.py
@@ -1,8 +1,13 @@
 from uuid import UUID
-
+from database import Database
 
 class User:
-    def __init__(self, username: str, uuid: UUID, role: str):
+    def __init__(self, username: str, uuid: UUID, role: str, email: str):
         self.username = username
         self.id = uuid
         self.role = role
+        self.email = email
+        self.inbox = []
+
+    def fill_inbox(self, db: Database):
+        notifications = db.get_notifications_by_user(self.id)
