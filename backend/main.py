02ab83d83753357a3ec2c8bf47aaf1fafe109f2a
Bradan Craig <bradancraig2004@gmail.com>
2025-12-01 11:39:05 -0500

    basic email functionality

--- backend/main.py
+++ backend/main.py
@@ -4,8 +4,8 @@ import os
 from dotenv import load_dotenv
 from database import Database
 from flask_session import Session
-from helpers import check_password, hash_password
-
+from helpers import *
+from notifications import Notification_Manager
 
 load_dotenv()
 
@@ -21,6 +21,7 @@ app.config["SESSION_COOKIE_SAMESITE"] = None
 
 
 db = Database()
+nm = Notification_Manager()
 
 
 @app.route("/authenticate", methods=["POST", "GET"])
@@ -29,19 +30,27 @@ def authenticate():
     username = data.get("username")
     password = data.get("password")
     user_information = db.get_user_by_username(username=username)
-    
+
     if check_password(
         origional_password=password, hashed_password=user_information["password"]
     ):
-        
         if user_information["role"] == "p":
             return "Account is still pending approval from admin"
         else:
             session["user"] = username
             session["role"] = user_information["role"]
-            session["id"] = str(user_information["_id"]) #Will get error if not type casted
+            session["id"] = str(
+                user_information["_id"]
+            )  # Will get error if not type casted
+
+            result = nm.send_unique_email(
+                receiver="bradan.craig@maine.edu",
+                subject="Testing",
+                message="This is a test message",
+            )
+            print(result)
             return jsonify({"message": "success"})
-    
+
     else:
         return jsonify({"message": "invalid credentials"})
 
@@ -61,6 +70,7 @@ def logout():
     session.pop("user", None)
     return jsonify({"message": "logged out"})
 
+
 @app.route("/register", methods=["POST"])
 def register():
     data = request.json
@@ -68,14 +78,15 @@ def register():
     hashed_password = hash_password(password=password)
     result = db.add_user(email=email, password=hashed_password)
     if result["result"]:
-        #send a notification to admin and update user management
+        # send a notification to admin and update user management
         _id = db.get_user_by_username(username=email)
         print(_id)
         db.set_user_role(id=_id["_id"], role="u")
         return jsonify({"message": "success"})
     else:
         return jsonify({"message": result["message"]})
-    
+
+
 # make sure to sanitize images for <script> tags, assigning UUID will happen in the back end
 if __name__ == "__main__":
-    app.run(debug=os.environ.get("FLASK_DEBUG"), port=5000)
+    app.run(debug=os.environ.get("FLASK_DEBUG"), port=5000, use_reloader=False)
--- backend/notifications.py
+++ backend/notifications.py
@@ -1,3 +1,31 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from dotenv import load_dotenv
+import os
+
+
 class Notification_Manager:
     def __init__(self):
-        self.email_address = ""  # We should make a email account so that email notifications can go through this
+        load_dotenv()
+        self.server = "smtp.gmail.com"
+        self.port = 587
+        self.email_address = "bradancraig2004@gmail.com"  # We should make a email account so that email notifications can go through this
+        self.email_password = os.environ.get("EMAIL_PASSWORD")
+
+    def send_unique_email(self, message: str, receiver: str, subject: str):
+        msg = MIMEMultipart()
+        msg["From"] = self.email_address
+        msg["To"] = receiver
+        msg["Subject"] = subject
+        msg.attach(MIMEText(message, "plain"))
+        try:
+            server = smtplib.SMTP(self.server, self.port)
+            server.starttls()
+            server.login(self.email_address, self.email_password)
+            server.sendmail(self.email_address, receiver, msg.as_string())
+            server.quit()
+            return "Email sent successfully!"
+
+        except Exception as e:
+            return f"Error sending email: {e}"
