33dd65bfdb14cd4ec7b1301250bebcd535242284
Teddy <teddyjmorin@gmail.com>
2026-02-09 10:23:51 -0500

    commiting to logging

--- backend/logger.py
+++ backend/logger.py
@@ -13,7 +13,7 @@ class SystemLogger:
             stream_handler.setFormatter(logging.Formatter('%(asctime)s - %(message)s'))
             
             self.logger.addHandler(file_handler)
-            self.logger.addHandler(stream_handler)
+            # self.logger.addHandler(stream_handler)
 
 
         logging.getLogger('werkzeug').setLevel(logging.ERROR) 
